3eb777d43a5712adb2742ee67e1fe89455c82ca3
srivinayaummeda-cmd <you@example.com>
2025-11-12 16:40:13 +0530

    practice

--- Practice/2.comparision operator.py
+++ Practice/2.comparision operator.py
@@ -2,15 +2,7 @@
 python operators
 -----------------
 '''
-#1.arthematic operators
-a=20
-b=10
-print("a+b:",a+b) #a+b:30
-print("a-b:",a-b) #a-b:10
-print("a*b:",a*b) #a*b:200
-print("a/b:",a/b) #a/b:2.0
-print("a//b:",a//b) #a//b:0
-print("a**b:",a**b)#a**b:10240000000000
+
 
 
 #2.comparision operators
